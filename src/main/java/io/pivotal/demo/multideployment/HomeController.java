8e08df911c96f6c35bf1dcbf0b21cc87dafe76e6
Michael McNichol <mcnichol.m@gmail.com>
2019-01-22 12:56:26 -0600

    Server Logo, Logic for detection

--- src/main/java/io/pivotal/demo/multideployment/HomeController.java
+++ src/main/java/io/pivotal/demo/multideployment/HomeController.java
@@ -1,5 +1,6 @@
 package io.pivotal.demo.multideployment;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -8,10 +9,14 @@ import java.util.Properties;
 
 @Controller
 public class HomeController {
+    @Value("${isEmbedded}")
+    private Boolean isEmbeddedServer;
 
     @RequestMapping("/")
     public ModelAndView getHome() {
         ModelAndView mav = new ModelAndView("index");
+
+        String hasPasProp = System.getProperty("CF_INSTANCE_GUID");
 
         String hasGlassfishProp = System.getProperty("glassfish.version");
         String hasJettyProp = System.getProperty("jetty.git.hash");
@@ -26,6 +31,8 @@ public class HomeController {
         mav.addObject("isJetty", hasJettyProp != null);
         mav.addObject("isGlassfish", hasGlassfishProp != null);
         mav.addObject("isTomcat", (hasTomcatProp != null && hasGlassfishProp == null));
+        mav.addObject("isEmbedded", isEmbeddedServer);
+        mav.addObject("isPas", hasPasProp != null);
 
         return mav;
     }
